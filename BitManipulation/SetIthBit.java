0a36884dc3d8cd91fe4f394858a8ec14df26f1d8
Naveen <naveen.jaisankar1999@gmail.com>
2025-08-20 00:16:15 +0100

    toggle the kth bit and remove the last set bit

--- BitManipulation/SetIthBit.java
+++ BitManipulation/SetIthBit.java
@@ -7,7 +7,11 @@ public class SetIthBit {
 
         int res = n | (1<<i); // this set ith bit to 1
         int res1 = n & ~(1<<i); // this set ith bit to 0
+        int res2 = n ^ (1<<i); // toggle the kth bit
+        int res3 = n & (n-1); //remove the last set bit
         System.out.println("Result is : " +  res);
         System.out.println("Result1 is : " +  res1);
+        System.out.println("Result2 is : " +  res2);
+        System.out.println("Result3 is : " +  res3);
     }
 }
