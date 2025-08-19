be60960da8fd2bfea1c61f66cc0774766778be9d
Naveen <naveen.jaisankar1999@gmail.com>
2025-08-20 00:42:57 +0100

    Revert "to check the number of set bits"
    This reverts commit eab4506b4901218f13f85a4ff2e4ccb6a8c3e836.

--- BitManipulation/SetIthBit.java
+++ BitManipulation/SetIthBit.java
@@ -2,18 +2,16 @@ package BitManipulation;
 
 public class SetIthBit {
     public static void main(String[] args) {
-        int n = 8; // 1001
+        int n = 9; // 1001
         int i = 2;
 
         int res = n | (1<<i); // this set ith bit to 1
         int res1 = n & ~(1<<i); // this set ith bit to 0
         int res2 = n ^ (1<<i); // toggle the kth bit
         int res3 = n & (n-1); //remove the last set bit
-        int res4 = n & (n-1); // check if the number is power of 2
         System.out.println("Result is : " +  res);
         System.out.println("Result1 is : " +  res1);
         System.out.println("Result2 is : " +  res2);
         System.out.println("Result3 is : " +  res3);
-        System.out.println( (res4==0) ? n + " is power of 2" : n + " is not power of 2");
     }
 }
